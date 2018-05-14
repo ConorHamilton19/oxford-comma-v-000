08b0fb037db59a975089dd4a415b5b9bfd094813
Conor Hamilton <conhamil19@gmail.com>
2018-05-14 10:34:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     "#{array[0]} and #{array[1]}"
   elsif array.length > 2 
-   array.join(", ")
+  array.pop
+  array.join(", ")
    
   end
       
